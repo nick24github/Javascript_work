854937d15ef32ab52a2d918d15639c498ac8bb88
Nikita Rawani <109915680+nick24github@users.noreply.github.com>
2024-05-07 09:30:32 +0000

    comparision file updated

--- 01_basics/03_conversionoroperation.js
+++ 01_basics/03_conversionoroperation.js
@@ -23,3 +23,10 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ****************** Operations ****************
+
+let value =3 
+let negValue = -value
+// console.log(negValue);
+
