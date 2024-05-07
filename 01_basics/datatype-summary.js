082bdfd0792513ec0c83cc207a95920058e0344c
Nikita Rawani <109915680+nick24github@users.noreply.github.com>
2024-05-07 09:59:20 +0000

    datatype summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,16 @@
+// Premitive
+
+// Array, Objects, Functions
+
+const herosArrays = ["shaktiman", "naagraj", "doga"]
+
+//Objects are in curly braces
+
+let myObj = {
+    name : "hitesh",
+    age : 22
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
